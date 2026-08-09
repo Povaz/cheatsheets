d37ab245a2a3fd88995c142ab72da859d9cf7fda
Erick Venneri <evenneri@gmail.com>
2026-08-09 20:57:35 +0200

    feat(view): vocabulary stylesheet on theme tokens

--- web/src/main.js
+++ web/src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router.js'
 import './index.css'
+import './styles/sheet.css'
 
 createApp(App).use(router).mount('#app')
